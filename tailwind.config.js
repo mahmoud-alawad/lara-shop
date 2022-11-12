24b60298f4331e7f1046f63bd79eef8925702513
mahmoud <awad25.ma@gmail.com>
2022-11-12 11:45:39 +0100

    tailwind general config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,27 @@ module.exports = {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-    "./resources/**/*.ts",
     "./resources/**/*.vue",
   ],
-   theme: {
-    extend: {},
+  theme: {
+    container: ({ theme }) => ({
+      center: true,
+      padding: theme('custom.spacing.gutter'),
+    }),
+    custom: {
+      spacing: {
+        gutter: '0.75rem',
+      },
+    },
+    extend: {
+      colors: {
+        primary: '#fd0000',
+        gray: "#636b6f",
+        'dark-gray': '#1b1b1b',
+        'medium-gray': '#292929',
+        'light-gray': '#d8d8d8',
+      },
+    },
   },
   plugins: [],
 }
